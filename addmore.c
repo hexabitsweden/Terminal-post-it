3352c419d898ec5ee108f538fff1f882c089067a
hexabit <daniel.jendinger@icloud.com>
2024-11-01 22:55:41 +0100

    Removed trailings.

--- addmore.c
+++ addmore.c
@@ -4,19 +4,19 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <stdlib.h>
-#define red   "\033[0;31m"        
-#define cyan  "\033[1;36m"      
-#define green "\033[0;32m"     
-#define blue  "\033[9;34m"  
-#define yellow "\e[1;33m" 
+#define red   "\033[0;31m"
+#define cyan  "\033[1;36m"
+#define green "\033[0;32m"
+#define blue  "\033[9;34m"
+#define yellow "\e[1;33m"
 #define black  "\033[9;34m"
 #define brown  "\033[0;33m"
 #define magenta  "\033[0;35m"
 #define gray  "\033[0;37m"
-#define bred  "\e[0;30;41m" 
+#define bred  "\e[0;30;41m"
 #define bgreen "\e[0;30;42m"
 #define byellow "\e[0;30;43m"
-#define none   "\033[0m" 
+#define none   "\033[0m"
 #define pink   "\e[1;30;31m"
 #define bblue  "\e[46m"
 #define bpurple  "\e[45m"
@@ -36,15 +36,15 @@ char inpt[20];
 char text[200];
 char text2[200];
  int  main(argc, argv)
- 
+
    int argc; char **argv;
       {
 
        strcpy(fname, argv[1]);
        i2 = strlen(fname)-1;
 
-       if( fname[ i2 ] == '\n') 
-          fname[i2] = '\0';  
+       if( fname[ i2 ] == '\n')
+          fname[i2] = '\0';
 
     FILE *fp;
     fp = fopen(fname, "a");
@@ -52,7 +52,7 @@ char text2[200];
     fgets(str, 150, stdin);
     i = strlen(str)-1;
 
-    if( str[ i ] == '\n') 
+    if( str[ i ] == '\n')
        str[i] = '\0';
 
     printf("%sOk wrote:%s %s\"%s to your note!\"%s\n",blue, none, yellow, str, none);
--- addnote.c
+++ addnote.c
@@ -4,19 +4,19 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <stdlib.h>
-#define red   "\033[0;31m"        
-#define cyan  "\033[1;36m"      
-#define green "\033[0;32m"     
-#define blue  "\033[9;34m"  
-#define yellow "\e[1;33m" 
+#define red   "\033[0;31m"
+#define cyan  "\033[1;36m"
+#define green "\033[0;32m"
+#define blue  "\033[9;34m"
+#define yellow "\e[1;33m"
 #define black  "\033[9;34m"
 #define brown  "\033[0;33m"
 #define magenta  "\033[0;35m"
 #define gray  "\033[0;37m"
-#define bred  "\e[0;30;41m" 
+#define bred  "\e[0;30;41m"
 #define bgreen "\e[0;30;42m"
 #define byellow "\e[0;30;43m"
-#define none   "\033[0m" 
+#define none   "\033[0m"
 #define pink   "\e[1;30;31m"
 #define bblue  "\e[46m"
 #define bpurple  "\e[45m"
@@ -38,7 +38,7 @@ char text2[200];
 
 
 
- writeIndex(string fname) 
+ writeIndex(string fname)
      {
     char danne[40];
     char danne2[40];
@@ -54,22 +54,22 @@ char text2[200];
       strcpy(fname,inpt);
       i2 = strlen(fname)-1;
 
-      if( fname[ i2 ] == '\n') 
-          fname[i2] = '\0';  
+      if( fname[ i2 ] == '\n')
+          fname[i2] = '\0';
 
     FILE *fp;
     fp = fopen(fname, "a");
     strcpy(str,text);
     i = strlen(str)-1;
 
-    if( str[ i ] == '\n') 
+    if( str[ i ] == '\n')
        str[i] = '\0';
 
     printf("%sOk wrote:%s %s\"%s to your note!\"%s\n",blue, none, yellow, str, none);
     fprintf(fp,"%s", str);
     fclose(fp);
  writeIndex(fname);
- 
+
 
  }
 
@@ -78,13 +78,13 @@ char text2[200];
  }
 
 int  main(argc, argv)
- 
+
    int argc; char **argv;
       {
         strcpy(text2, argv[2]);
         strcpy(text, argv[3]);
         strcpy(inpt, argv[1]);
-        strcpy(tester, "add"); 
+        strcpy(tester, "add");
         if (argv[1]!=NULL) {
             cmds(text2, text);
                donner();
@@ -96,8 +96,8 @@ int  main(argc, argv)
        strcpy(fname,note);
        i2 = strlen(fname)-1;
 
-       if( fname[ i2 ] == '\n') 
-          fname[i2] = '\0';  
+       if( fname[ i2 ] == '\n')
+          fname[i2] = '\0';
 
     FILE *fp;
     fp = fopen(fname, "a");
@@ -105,7 +105,7 @@ int  main(argc, argv)
     fgets(str, 150, stdin);
     i = strlen(str)-1;
 
-    if( str[ i ] == '\n') 
+    if( str[ i ] == '\n')
        str[i] = '\0';
 
     printf("%sOk wrote:%s %s\"%s to your note!\"%s\n",blue, none, yellow, str, none);
